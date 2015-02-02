de4771c5ff5f5d35f14170bcd67960c38f0d6493
toxohut <kamitoxohut@gmail.com>
2015-02-02 11:39:13 +0100

    poprawienie filtrowania na custom, nie ma za dobrej obslugi bledow

--- Kalendarz Kraków Arena/Historia.xaml.cs
+++ Kalendarz Kraków Arena/Historia.xaml.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -21,6 +22,10 @@ namespace Kalendarz_Kraków_Arena
     /// </summary>
     public partial class Historia : Window
     {
+        DataTable dt = new DataTable();
+        DataView dv = new DataView();
+        
+
         public Historia()
         {
             InitializeComponent();
@@ -36,12 +41,21 @@ namespace Kalendarz_Kraków_Arena
                 connection.Open();
                 using (MySqlCommand cmdSel = new MySqlCommand(sql, connection))
                 {
-                    DataTable dt = new DataTable();                    
+                    dt = new DataTable();
                     MySqlDataAdapter da = new MySqlDataAdapter(cmdSel);
                     da.Fill(dt);                    
                     DataGrid.ItemsSource = dt.DefaultView;
                 }
                 connection.Close();
+
+                string[] columnNames = dt.Columns.Cast<DataColumn>()//wypelnianie combobox
+                                 .Select(x => x.ColumnName)
+                                 .ToArray();
+                for (int i = 0; i < columnNames.Length; i++)
+                {
+                    ColumsBox.Items.Add(columnNames[i]);
+                }
+                ColumsBox.SelectedIndex = 0;
             }
         }
 
@@ -55,18 +69,46 @@ namespace Kalendarz_Kraków_Arena
                 connection.Open();
                 using (MySqlCommand cmdSel = new MySqlCommand(sql, connection))
                 {
-                    DataTable dt = new DataTable();                    
+                    dt = new DataTable();
                     MySqlDataAdapter da = new MySqlDataAdapter(cmdSel);
                     da.Fill(dt);                    
                     DataGrid.ItemsSource = dt.DefaultView;
                 }
                 connection.Close();
+
+                string[] columnNames = dt.Columns.Cast<DataColumn>()//wypelnianie combobox
+                                 .Select(x => x.ColumnName)
+                                 .ToArray();
+                for (int i = 0; i < columnNames.Length; i++)
+                {
+                    ColumsBox.Items.Add(columnNames[i]);
+                }
+                ColumsBox.SelectedIndex = 0;
             }
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)//funkcja odpowiedzialna za filtrowanie
+        {
+            //(dataGridViewFields.DataSource as DataTable).DefaultView.RowFilter = string.Format("Field = '{0}'", TextBox.Text);           
+            dv = new DataView(dt);            
+            string klucz;
+            string wartosc;
+
+            klucz = ColumsBox.SelectedItem.ToString();
+            wartosc = FilterBox.Text;
+            
+            if (klucz != "" && wartosc != "")//to nie do konca dziala trzeba tutaj dosrac jeszcze duzo warunkow
+            {
+                MessageBox.Show("" + klucz + " = " + wartosc + "");
+                dv.RowFilter = "" + klucz + " = " + wartosc + "";
+            }
+            //dv.RowFilter = "Login=2";
+            DataGrid.ItemsSource = dv;
         }
     }
 }
